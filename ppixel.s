4386c0de8855a2824da5c4c451f800587ad28975
honey data <16432726+honeydatax@users.noreply.github.com>
2018-09-10 21:55:00 +0100

    background color sample

--- ppixel.s
+++ ppixel.s
@@ -8,6 +8,11 @@ clear6: .word	50
 gotoxy1: .asciz "%c[%d;%df"
 gotoxy2: .word	0
 gotoxy3: .word	1
+backa: .word	639
+backb: .word	479
+backred: .word 0
+backgreen: .word 0
+backblue: .word 0
 msg: .asciz "im love marina"
 a: .word	639
 b: .word	478
@@ -23,45 +28,74 @@ c: .word	5000000
 .extern endX
 .extern usleep
 
+back:
+	push {ip,lr}
+	ldr r4,=backred
+	str r0,[r4]
+	
+	ldr r4,=backgreen
+	str r1,[r4]
+	
+	ldr r4,=backblue
+	str r2,[r4]
 
-
-
-main:
-
-	bl startX
 	mov r10,#0
 	mov r8,#0
 	mov r7,#0
 
-	ldr r11,=a
-	ldr r9,=b
+	ldr r11,=backa
+	ldr r9,=backb
 	ldr r11,[r11]
 	ldr r9,[r9]
 	mov r6,#1
 
 
-for1:
+back1:
+
+
+	ldr r2,=backred
+	ldr r2,[r2]
+	
+	ldr r3,=backgreen
+	ldr r3,[r3]
+	
+	ldr r4,=backblue
+	ldr r4,[r4]
+	
+	
+	
 	mov r0,r8
 	mov r1,r7
-	mov r2,#200
-	mov r3,#128
-	mov r4,#0
+	
 
 	bl ppixel
 
+	mov r6,#1
 	add r8,r8,r6
 	cmp r8,r11
-	bne for2
+	bne back2
 	
 	add r7,r7,r6
 	mov r8,#0
-for2:
+back2:
 	
 	cmp r7,r9
-	beq exitss
+	beq back5
 	
-	bne for1
+	bne back1
 
+back5:
+	pop {ip,pc}
+
+main:
+
+	bl startX
+	mov r0,#200
+	mov r1,#128
+	mov r2,#0
+	
+	bl back
+	
 exitss:
 	ldr r0,=c
 	ldr r0,[r0]
