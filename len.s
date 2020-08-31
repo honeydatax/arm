51dcef3fae749fea698e36e1f362e16d19fb656d
lisbon <16432726+honeydatax@users.noreply.github.com>
2020-08-31 19:30:50 +0100

    Add files via upload

---
+++ len.s
@@ -0,0 +1,88 @@
+	.arch armv6
+	.eabi_attribute 27, 3
+	.eabi_attribute 28, 1
+	.fpu vfp
+	.eabi_attribute 20, 1
+	.eabi_attribute 21, 1
+	.eabi_attribute 23, 3
+	.eabi_attribute 24, 1
+	.eabi_attribute 25, 1
+	.eabi_attribute 26, 2
+	.eabi_attribute 30, 6
+	.eabi_attribute 34, 1
+	.eabi_attribute 18, 4
+	.file	"len.c"
+	.text
+	.align	2
+	.global	strslen
+	.type	strslen, %function
+strslen:
+	@ args = 0, pretend = 0, frame = 16
+	@ frame_needed = 1, uses_anonymous_args = 0
+	@ link register save eliminated.
+	str	fp, [sp, #-4]!
+	add	fp, sp, #0
+	sub	sp, sp, #20
+	str	r0, [fp, #-16]
+	mov	r3, #0
+	str	r3, [fp, #-8]
+	b	.L2
+.L3:
+	ldr	r3, [fp, #-8]
+	add	r3, r3, #1
+	str	r3, [fp, #-8]
+.L2:
+	ldr	r3, [fp, #-8]
+	ldr	r2, [fp, #-16]
+	add	r3, r2, r3
+	ldrb	r3, [r3]	@ zero_extendqisi2
+	cmp	r3, #0
+	bne	.L3
+	ldr	r3, [fp, #-8]
+	mov	r0, r3
+	sub	sp, fp, #0
+	@ sp needed
+	ldr	fp, [sp], #4
+	bx	lr
+	.size	strslen, .-strslen
+	.section	.rodata
+	.align	2
+.LC0:
+	.ascii	"hello world\012\000"
+	.align	2
+.LC1:
+	.ascii	"%s %d \012\000"
+	.text
+	.align	2
+	.global	main
+	.type	main, %function
+main:
+	@ args = 0, pretend = 0, frame = 8
+	@ frame_needed = 1, uses_anonymous_args = 0
+	stmfd	sp!, {fp, lr}
+	add	fp, sp, #4
+	sub	sp, sp, #8
+	mov	r3, #0
+	str	r3, [fp, #-8]
+	ldr	r3, .L7
+	str	r3, [fp, #-12]
+	ldr	r0, [fp, #-12]
+	bl	strslen
+	str	r0, [fp, #-8]
+	ldr	r0, .L7+4
+	ldr	r1, [fp, #-12]
+	ldr	r2, [fp, #-8]
+	bl	printf
+	mov	r3, #0
+	mov	r0, r3
+	sub	sp, fp, #4
+	@ sp needed
+	ldmfd	sp!, {fp, pc}
+.L8:
+	.align	2
+.L7:
+	.word	.LC0
+	.word	.LC1
+	.size	main, .-main
+	.ident	"GCC: (Raspbian 4.9.2-10+deb8u2) 4.9.2"
+	.section	.note.GNU-stack,"",%progbits
